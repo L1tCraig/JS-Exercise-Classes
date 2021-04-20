5056170e7d60ce49380c383e3a8b902ab136da26
L1tCraig <L1tCraigJ@gmail.com>
2021-04-20 01:40:41 -0500

    37 passing

--- index.js
+++ index.js
@@ -169,8 +169,13 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian{
+  constructor(info){
+    super(info);
+    this.previousBackground = info.previousBackground;
+    this.className = info.className;
+    this.favSubjects = info.favSubjects;
+  }
  }
   
   /*
